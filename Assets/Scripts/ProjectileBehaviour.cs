09d525e8fb31243c86ea37350d2681c5084dba93
PLAYTIKA\dzmitryis <dzmitryis@playtika.com>
2022-04-06 16:12:37 +0300

    Pooling task done

---
+++ Assets/Scripts/ProjectileBehaviour.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+public class ProjectileBehaviour : MonoBehaviour
+{
+	[Header("Movement")]
+	public float speed = 50f;
+
+	public float bulletLifeTime = 5f;
+
+	public PlayerShooting playerShooting;
+
+	private void OnEnable()
+	{
+		StartCoroutine(RemoveOverTime(bulletLifeTime));
+	}
+
+	private void OnDisable()
+	{
+		StopAllCoroutines();
+	}
+
+	void Update()
+	{
+		Vector3 movement = transform.forward * speed * Time.deltaTime;
+		GetComponent<Rigidbody>().MovePosition(transform.position + movement);
+	}
+
+	void OnTriggerEnter(Collider theCollider)
+	{
+		if (theCollider.tag == "Enemy" || theCollider.tag == "Environment")
+			RemoveProjectile();
+	}
+
+	void RemoveProjectile()
+	{
+		playerShooting.DisableObject(gameObject);
+
+	}
+
+	IEnumerator RemoveOverTime(float time)
+	{
+		yield return new WaitForSeconds(time);
+		RemoveProjectile();
+	}
+}
